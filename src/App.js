2588d337cedc78007891b3949a230abe54d6a9be
kspiteri <contact@keithspiteri.com>
2022-07-26 17:13:26 +0200

    add authentication

--- src/App.js
+++ src/App.js
@@ -1,29 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import Auth from "./components/Auth";
 import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
 import { Banner, Details, WhatToDo } from "./components/Sections";
-import ScrollToElement from "./components/ScrollToElement";
-import Logo from "./components/Logo";
 
 function App() {
-  return (
-    <div className="app" id="top">
-        <NavBar />
-        <main>
-            <Banner />
-            <Details />
-            <WhatToDo />
-        </main>
-        <footer>
-            <div className="col-sm-12 text-center content">
-                <span className="to-top-wrapper">
-                    <ScrollToElement id="top" className="to-top">&#8963;</ScrollToElement>
-                </span>
-                <h4 className="footer-text"><Logo /></h4>
-                <p className="footer-text">Designed and developed by Keith with lots of <span className="footer-text-heart">&#9829;</span> for Becca</p>
-            </div>
-        </footer>
-    </div>
-  );
+    const [isAuth, auth] = useState(false);
+
+    if (!isAuth) return <Auth event={auth} />;
+
+    return (
+        <div className="app" id="top">
+            <NavBar />
+            <main>
+                <Banner />
+                <Details />
+                <WhatToDo />
+            </main>
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
